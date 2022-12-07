14ce390dbe769e9d33be43ca9a03787871aa40ae
JRisJunior <jeffmrubin@gmail.com>
2022-12-07 13:07:38 -0600

    post new hardcode product successfully to database

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,14 @@ class ProductsController < ApplicationController
     render json: product.as_json
   end
   def create
-    render json: {message: "Hello!"}
+    product = Product.new(
+      name: "hackey sack",
+      price: 4,
+      image_url: "https://worldfootbag.com/shop/wp-content/uploads/2011/05/Sipa-Sipa-Rasta-Main.jpg",
+      description: "use this to hack some sack."
+    )
+    product.save
+    render json: product.as_json
   end
 
 end
