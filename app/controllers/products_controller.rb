a5d800790e7f63761659380f915d5cb2c6921a7d
JRisJunior <jeffmrubin@gmail.com>
2022-12-08 14:21:31 -0600

    create friendly_created_at method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,14 @@
 class ProductsController < ApplicationController
   def index
     products = Product.all
-    render json: products.as_json
+    render json: products.as_json(methods: [:friendly_created_at])
   end
+
   def show
     product = Product.find_by(id: params[:id])
-    render json: product.as_json
+    render json: product.as_json(methods: [:friendly_created_at])
   end
+
   def create
     # puts "What is the product you would like to add?"
     # name = gets.chomp
