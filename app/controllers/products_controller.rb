c9e84182179d570bafb09e198d64ed73052b33a9
JRisJunior <jeffmrubin@gmail.com>
2022-12-12 13:45:31 -0600

    add happy sad paths for create action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -32,8 +32,12 @@ class ProductsController < ApplicationController
       image_url: params[:image_url],
       description: params[:description]
     )
-    product.save
-    render json: product.as_json
+    if @product.save
+      render json: @product.as_json
+    else
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
+    
   end
 
   def update
