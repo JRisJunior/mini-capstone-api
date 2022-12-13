7de1a7092af4c6e85ce0dd56c5c3de4cf5006cd2
JRisJunior <jeffmrubin@gmail.com>
2022-12-13 13:55:13 -0600

    add image_id column to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_13_194859) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_13_195423) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_194859) do
     t.datetime "updated_at", null: false
     t.integer "inventory"
     t.integer "supplier_id"
+    t.string "image_id"
   end
 
   create_table "suppliers", force: :cascade do |t|
