5c0c46c1647ca065c45e91363ebd76c90537d42c
JRisJunior <jeffmrubin@gmail.com>
2022-12-13 13:58:07 -0600

    remove image_url column from products model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_13_195423) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_13_195715) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +23,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_195423) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price", precision: 9, scale: 2
-    t.string "image_url"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
