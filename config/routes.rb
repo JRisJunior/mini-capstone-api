7e5fddeca983752e775becf69e7fb43314807687
JRisJunior <jeffmrubin@gmail.com>
2022-12-14 12:34:21 -0600

    add BCrypt gem + remove image_id from products

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ get "/products/:id" => "products#show"
 post "/products" => "products#create"
 patch "/products/:id" => "products#update"
 delete "/products/:id" => "products#destroy"
+
+post "/users" => "users#create"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_13_195715) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_14_183304) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,13 +28,20 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_195715) do
     t.datetime "updated_at", null: false
     t.integer "inventory"
     t.integer "supplier_id"
-    t.string "image_id"
   end
 
   create_table "suppliers", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
