7060e93cbd2994382f3f3243964de1ed637e4381
JRisJunior <jeffmrubin@gmail.com>
2023-01-26 14:15:10 -0600

    change user auth to be easier + add localhost 8080

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "mini-capstone.peterxjang.com"
+    origins "http://localhost:5173"
+    origins "http://localhost:8080"
 
     resource "*",
       headers: :any,
